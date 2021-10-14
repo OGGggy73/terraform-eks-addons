94c6960be6196ec1be99169183954061e1c90517
Anunay <anunaykumar73@gmail.com>
2021-10-15 04:17:08 +0530

    adding ingress module

--- main.tf
+++ main.tf
@@ -11,3 +11,9 @@ module "metrics-server" {
     source = "./modules/metrics-server"
     count = var.metrics_server_enabled ? 1 : 0
 }
+
+module "ingress-nginx" {
+    source = "./modules/ingress-nginx"
+    count = var.ingress_nginx_enabled ? 1 : 0
+    additional_set = var.ingress_nginx_additional_sets
+}
