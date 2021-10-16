32bad598803096f9f1ef16f3bab5664ecf9627d4
Anunay <anunaykumar73@gmail.com>
2021-10-16 21:27:22 +0530

    added external secrets module

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+data "aws_region" "current" {}
+
 module "cluster-autoscaler" {
     source = "./modules/cluster-autoscaler"
     count = var.cluster_autoscaler_enabled ? 1 : 0
@@ -16,4 +18,13 @@ module "ingress-nginx" {
     source = "./modules/ingress-nginx"
     count = var.ingress_nginx_enabled ? 1 : 0
     additional_set = var.ingress_nginx_additional_sets
+}
+
+module "external-secrets" {
+    source = "./modules/external-secrets"
+    count = var.external_secrets_enabled ? 1 : 0
+    cluster_name                     = var.cluster_name
+    cluster_identity_oidc_issuer     = var.cluster_identity_oidc_issuer
+    cluster_identity_oidc_issuer_arn = var.cluster_identity_oidc_issuer_arn
+    secrets_aws_region               = data.aws_region.current.name
 }
